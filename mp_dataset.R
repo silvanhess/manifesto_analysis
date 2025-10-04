ad0117440392dd107882663e20b537e258b680cc
Silvan Hess <silvan.hess@stud.unilu.ch>
2025-10-04 22:41:37 +0200

    mp dataset creation

---
+++ mp_dataset.R
@@ -0,0 +1,48 @@
+# manifesto project dataset creation using the R package
+
+# load packages
+library(manifestoR)
+library(tidyverse)
+library(countrycode)
+
+# inspect manifesto project package
+??manifestoR
+manifestoR::mp_setapikey("manifesto_apikey.txt")
+versions <- manifestoR::mp_coreversions()
+codebook <- manifestoR::mp_codebook()
+maindataset <- manifestoR::mp_maindataset()
+parties <- manifestoR::mp_parties()
+
+# Create Dataset
+
+# 1. Filter European manifestos after 2000
+european_manifestos <- maindataset |>
+  mutate(continent = countrycode(countryname, "country.name", "continent")) |>
+  filter(continent == "Europe", date > 200000)
+
+# 2. Keep only greens and populists
+greens_populists <- european_manifestos |>
+  filter(parfam %in% c(10, 70))
+
+# 3. Keep only countries that have BOTH groups
+greens_populists_filtered <- greens_populists |>
+  group_by(countryname) |>
+  filter(all(c(10, 70) %in% parfam)) |>
+  ungroup()
+
+# 4. Create election periods
+greens_populists_periods <-
+  greens_populists_filtered |>
+  mutate(
+    year = date %/% 100, # extract year from YYYYMM
+    period = case_when(
+      year >= 2000 & year <= 2004 ~ "2000-2004",
+      year >= 2005 & year <= 2009 ~ "2005-2009",
+      year >= 2010 & year <= 2014 ~ "2010-2014",
+      year >= 2015 & year <= 2019 ~ "2015-2019",
+      year >= 2020 & year <= 2025 ~ "2020-2025",
+      TRUE ~ NA_character_
+    )
+  )
+
+write_rds(greens_populists_periods, "greens_populists_periods.rds")
